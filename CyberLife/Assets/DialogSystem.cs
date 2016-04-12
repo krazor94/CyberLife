90bec0ee42ba307eff32b4be96e204902282ba1f
krazor94 <jakeaquilina94@gmail.com>
2016-04-13 00:00:43 +1000

    fixed Day Night cycle (Still needs some work) implemented Dialog System Template

---
+++ CyberLife/Assets/DialogSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogSystem : MonoBehaviour
+{
+    [System.Serializable]
+    public class Dialog
+    {
+        public bool Complete;
+        public string text;
+        public GameObject human;
+        public GameObject Robot;
+    };
+
+    public List<Dialog> Dialogs = new List<Dialog>();
+    
+    public Text text;
+    public float incChar, nextChar;
+    public int c, totalChars;
+    public bool FillDialog;
+
+    void Start()
+    {
+        totalChars = Dialogs[0].text.Length;
+    }
+
+    
+    void FixedUpdate()
+    {
+
+        if (FillDialog)
+        {
+            if (c < totalChars)
+            {
+                if (Time.time > nextChar)
+                {
+                    text.text = text.text + Dialogs[0].text[c];
+                    c++;
+                    nextChar = Time.time + 0.1f;
+                }
+            }
+        }
+    }
+}
--- CyberLife/Assets/Scripts/DayNightCycle.cs
+++ CyberLife/Assets/Scripts/DayNightCycle.cs
@@ -3,42 +3,69 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    public float day, hr, min, sec, nextTime;
-    public float timeOfDay, daySpeed;
+    public float day, hr, min;
+    public float timeOfDay, daySpeedModifier;
 
     public float smooth = 2.0F;
 
-    private void Update()
+
+    public int minMorning, maxMorning, 
+               minNoon, maxNoon, 
+               minEvening, maxEvening, 
+               minNight, maxNight;
+
+
+    private void FixedUpdate()
     {
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(timeOfDay, 0, 0), Time.deltaTime * smooth);
 
-        if (Time.time > nextTime)
+
+        /*
+         5AM to 7AM - Dawn
+
+        11AM - 1PM  -  Noon
+        
+        5pm to 7PM -   Duck and,
+        
+        8PM till 4AM is Night
+         
+         */
+
+
+
+        if (hr >= minMorning && hr < maxMorning)
         {
-            sec += 1 + daySpeed;
-            nextTime = Time.time + 1;
+            timeOfDay = 5;
         }
-
-        if (sec >= 60)
+        else if (hr >= minNoon && hr < maxNoon)
         {
-            min++;
-            sec = 0;
+            timeOfDay = 90;
+        }
+        else if (hr >= minEvening && hr < maxEvening)
+        {
+            timeOfDay = 170;
+        }
+         else if (hr >= minNight)
+        {
+            timeOfDay = 290;
+        }
+        
 
-            if (min >= 60)
+
+
+
+
+
+        min += 0.35f + daySpeedModifier;
+        if (min >= 60)
+        {
+            hr++;
+            min = 0;
+
+            if (hr >= 24)
             {
-                hr++;
-                min = 0;
-
-                if (hr >= 12 && hr <= 6)
-                    timeOfDay += 7.5f;
-                else
-                    timeOfDay += 22;
-
-                if (hr > 24)
-                {
-                    hr = 0;
-                    timeOfDay = 0;
-                    day++;
-                }
+                hr = 0;
+                day++;
             }
         }
 
