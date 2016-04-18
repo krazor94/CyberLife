5535282795b392b9ca55137e00c0dd1008b09ff5
Matthew Malin <1002308@student.sae.edu.au>
2016-04-18 15:13:35 +1000

    Added Basic models, House models, Animations, Character Model (with the ability to change limb sections) and placed the walls using 3D boxes to create an area where the sun's light can not go through but is also hidden for the player as well.

---
+++ CyberLife/Assets/DoorAnimations.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimations : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider col)
+	{
+
+		if (col.tag == "Player") 
+		{
+			transform.GetChild(0).GetComponent<Animator> ().SetBool ("Door", true);
+		}
+
+	}
+	void OnTriggerExit(Collider col)
+	{
+
+		if (col.tag == "Player") 
+		{
+			transform.GetChild(0).GetComponent<Animator> ().SetBool ("Door", false);
+		}
+
+	}
+
+
+}
